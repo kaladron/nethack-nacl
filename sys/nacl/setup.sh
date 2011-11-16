1f946c299ec1dc06bf2b9636a957339d83e1dc77
Jeff Bailey <jbailey@raspberryginger.com>
2011-11-15 22:47:13 -0800

    Link in hello-world dart script

--- sys/nacl/setup.sh
+++ sys/nacl/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export NACL_SDK_ROOT=/home/jeffbailey/Programming/svntree/naclports/src/
-export PATH=${NACL_SDK_ROOT}/toolchain/linux_x86_newlib/bin:$PATH
+export DART=/home/jeffbailey/Programming/gclienttree/dart/dart/frog/
+export PATH=${DART}:${NACL_SDK_ROOT}/toolchain/linux_x86_newlib/bin:$PATH
