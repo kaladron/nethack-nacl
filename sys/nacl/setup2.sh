1d7b0931cc7274959f3a242ea5776de38f28f0eb
Brad Nelson <bradnelson@google.com>
2011-11-16 10:01:00 -0800

    Dart -> frog

--- sys/nacl/setup2.sh
+++ sys/nacl/setup2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export NACL_SDK_ROOT=~/clients/naclports/src
-export DART_DIR=~/clients/dart/dart/frog
+export FROG_DIR=~/clients/dart/dart/frog
 export PATH=${NACL_SDK_ROOT}/toolchain/mac_x86_newlib/bin:${FROG_DIR}:$PATH
