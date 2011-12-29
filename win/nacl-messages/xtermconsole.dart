55f633f83999bf38100b670c08ec77cb916fde15
Jeff Bailey <jbailey@raspberryginger.com>
2011-12-28 23:06:18 -0800

    Basic display in xterm

--- win/nacl-messages/xtermconsole.dart
+++ win/nacl-messages/xtermconsole.dart
@@ -13,12 +13,55 @@ interface NethackUi {
 class XtermConsole {
   String windowtype = "tty";
 
-  void setup() {
+  int width = 80;
+  int height = 24;
 
+  int cursor_x = 0;
+  int cursor_y = 0;
+
+  PreElement pre;
+
+  void setup() {
+    DivElement game = document.query("#game");
+    pre = new Element.tag("pre");
+
+    for (int y = 0; y < height; y++) {
+      SpanElement r = new Element.tag("span");
+      r.classes = ['x-line'];
+
+      for (int x = 0; x < width; x++) {
+        r.nodes.add(createCell());
+      }
+
+      pre.nodes.add(r);
+    }
+
+    game.nodes.add(pre);
+  }
+
+  Element createCell() {
+    SpanElement c = new Element.tag("span");
+    c.text = ' ';
+    return c;
+  }
+
+  void putString(s) {
+    for (int i = 0; i < s.length; i++) {
+      if (cursor_x >= width) {
+        cursor_x = 0;
+        cursor_y++;
+      }
+      if (cursor_y >= height) {
+        return;
+      }
+      SpanElement cell = pre.nodes[cursor_y].nodes[cursor_x];
+      cell.text = s[i];
+      cursor_x++;
+    }
   }
 
   void handleMessage(var msg) {
-    print(msg.data);
+    putString(msg.data);
   }
 }
 
