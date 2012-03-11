3b9db382f410dd810ce2f8dd24a68b88431cb478
Jeff Bailey <jbailey@raspberryginger.com>
2012-03-11 09:16:41 -0700

    Attempt to fixup Brad's setup2.sh

--- sys/nacl/setup2.sh
+++ sys/nacl/setup2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export NACL_SDK_ROOT=~/clients/naclports/src
-export FROG_DIR=~/clients/dart/dart/frog
+export FROG_DIR=~/clients/dart/dart/out/Debug_ia32/dart-sdk/bin
 export PATH=${NACL_SDK_ROOT}/toolchain/mac_x86_newlib/bin:${FROG_DIR}:$PATH
