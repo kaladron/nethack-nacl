6be39ba391c6245b1fc6e9665e25f0e28e079f24
Jeff Bailey <jbailey@raspberryginger.com>
2012-03-29 15:56:44 -0700

    Override glibc's getuid so that we have a static number between newlib and glibc

---
+++ sys/nacl/nacl_shim.cc
@@ -0,0 +1,30 @@
+extern "C" {
+
+#include "hack.h"
+
+char erase_char, kill_char;
+
+void gettty() { }
+
+void settty(const char* unused) { }
+
+void setftty() {
+  iflags.cbreak = ON;
+  iflags.echo = OFF;
+}
+
+void setctty() { }
+
+}
+
+void introff() { }
+
+void intron() { }
+
+int isatty(int fd) {
+  return 1;
+}
+
+uid_t getuid() {
+  return 1001;
+}
