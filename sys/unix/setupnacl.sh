3b071961520a3cd8e7676b3a16c4af3d95472c2e
Jeff Bailey <jeffbailey@google.com>
2012-08-30 17:23:10 -0700

    When handed an argument, make symlinks for the Makefiles

--- sys/unix/setupnacl.sh
+++ sys/unix/setupnacl.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 # Copy files to their correct locations.
 
+if [ -f sys/unix/Makefile.top ]; then cd sys/unix; fi
+
 echo "Copying Makefiles."
 
 [ -d ../../obj32 ] || mkdir ../../obj32
 [ -d ../../obj64 ] || mkdir ../../obj64
+
+if [ $# -gt 0 ] ; then
+	ln -s sys/unix/Makefile.nacl ../../Makefile
+	ln -s ../sys/unix/Makefile.dat ../../dat/Makefile
+	ln -s ../sys/unix/Makefile.doc ../../doc/Makefile
+	ln -s ../sys/unix/Makefile.utl ../../util/Makefile
+
+	ln -s ../sys/unix/Makefile.src ../../obj32/Makefile
+	ln -s ../sys/unix/Makefile.src ../../obj64/Makefile
+	exit
+fi
 
 cp Makefile.nacl ../../Makefile
 cp Makefile.dat ../../dat/Makefile
