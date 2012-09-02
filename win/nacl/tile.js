ecf0c6bdf09489dd21540395d2a5f8a95a46bb88
Jeff Bailey <jeffbailey@google.com>
2012-09-02 14:07:20 -0700

    Remove extra inclusion of nethack.js, add initial tile handler.

---
+++ win/nacl/tile.js
@@ -0,0 +1,23 @@
+startGame = function() {
+
+  // Create the object for Nethack.
+  nethackEmbed = document.createElement('object');
+  nethackEmbed.width = 0;
+  nethackEmbed.height = 0;
+  nethackEmbed.addEventListener('message', handleMessage);
+  nethackEmbed.data = 'nethack.nmf';
+  nethackEmbed.type = 'application/x-nacl';
+
+  var param = document.createElement('param');
+  param.name = 'windowtype';
+  param.value = 'nacl';
+  nethackEmbed.appendChild(param);
+
+  document.getElementById('listener').appendChild(nethackEmbed);
+}
+
+handleMessage = function(event) {
+  console.log(event);
+}
+
+startGame();
