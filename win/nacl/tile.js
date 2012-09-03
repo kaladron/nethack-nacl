41aef84c6d76de541a2461e4cdc19100ce2759d1
Jeff Bailey <jeffbailey@google.com>
2012-09-02 22:59:26 -0700

    Add constants for tile messages.  Add message handler and first response to name to prove round-trip

--- win/nacl/tile.js
+++ win/nacl/tile.js
@@ -1,3 +1,47 @@
+var NaclMsg = {};
+NaclMsg.INIT_NHWINDOWS = 0;
+NaclMsg.PLAYER_SELECTION = 1;
+NaclMsg.ASKNAME = 2;
+NaclMsg.GET_NH_EVENT = 3;
+NaclMsg.EXIT_NHWINDOWS = 4;
+NaclMsg.SUSPEND_NHWINDOWS = 5;
+NaclMsg.RESUME_NHWINDOWS = 6;
+NaclMsg.CREATE_NHWINDOW = 7;
+NaclMsg.CREATE_NHWINDOW_BY_ID = 8;
+NaclMsg.CLEAR_NHWINDOW = 9;
+NaclMsg.DISPLAY_NHWINDOW = 10;
+NaclMsg.DESTROY_NHWINDOW = 11;
+NaclMsg.CURS = 12;
+NaclMsg.PUTSTR = 13;
+NaclMsg.DISPLAY_FILE = 14;
+NaclMsg.START_MENU = 15;
+NaclMsg.ADD_MENU = 16;
+NaclMsg.END_MENU = 17;
+NaclMsg.SELECT_MENU = 18;
+NaclMsg.UPDATE_INVENTORY = 19;
+NaclMsg.MARK_SYNCH = 20;
+NaclMsg.WAIT_SYNCH = 21;
+NaclMsg.CLIPAROUND = 22;
+NaclMsg.CLIPAROUND_PROPER = 23;
+NaclMsg.PRINT_GLYPH = 24;
+NaclMsg.RAW_PRINT = 25;
+NaclMsg.RAW_PRINT_BOLD = 26;
+NaclMsg.NHGETCH = 27;
+NaclMsg.NH_POSKEY = 28;
+NaclMsg.NHBELL = 29;
+NaclMsg.DOPREV_MESSAGE = 30;
+NaclMsg.YN_FUNCTION = 31;
+NaclMsg.GETLIN = 32;
+NaclMsg.GET_EXT_CMD = 33;
+NaclMsg.NUMBER_PAD = 34;
+NaclMsg.DELAY_OUTPUT = 35;
+NaclMsg.START_SCREEN = 36;
+NaclMsg.END_SCREEN = 37;
+NaclMsg.OUTRIP = 38;
+NaclMsg.DELETE_NHWINDOW_BY_REFERENCE = 39;
+
+var nethackEmbed;
+
 startGame = function() {
 
   // Create the object for Nethack.
@@ -16,8 +60,24 @@ startGame = function() {
   document.getElementById('listener').appendChild(nethackEmbed);
 }
 
+var PREFIX = 'JSPipeMount:3:';
+
+function pm(out) {
+  nethackEmbed.postMessage(PREFIX + out);
+}
+
 handleMessage = function(event) {
-  console.log(event);
+  // Make sure it's the right kind of event we got
+  // Check to make sure it starts with PREFIX
+  var msg = JSON.parse(event.data.substr(PREFIX.length));
+  console.log(msg);
+  switch(msg[0]) {
+  case NaclMsg.INIT_NHWINDOWS:
+    break;
+  case NaclMsg.ASKNAME:
+    pm('bob');
+    break;
+  }
 }
 
 startGame();
