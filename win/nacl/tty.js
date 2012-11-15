459b01f63bac6bf76c2b6e3e8987bfa60ca9cbe5
Jeff Bailey <jbailey@raspberryginger.com>
2012-11-14 23:04:14 -0800

    Make drop down stay up for 3 seconds

--- win/nacl/tty.js
+++ win/nacl/tty.js
@@ -2,3 +2,9 @@
 
 lib.ensureRuntimeDependencies();
 hterm.init(Nethack.init);
+
+function runit() {
+  document.getElementById('header').classList.remove('header-initial');
+}
+
+setTimeout(runit, 3000);
