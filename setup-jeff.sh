a6f8bdc2e61c41d04766f5de16e0d74854991c92
Jeff Bailey <jeffbailey@google.com>
2013-06-06 15:20:41 -0700

    Update for pepper 27

--- setup-jeff.sh
+++ setup-jeff.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export NACL_SDK_ROOT=/home/jeffbailey/Programming/nacl_sdk/pepper_20
+export NACL_SDK_ROOT=${HOME}/Programming/nacl_sdk/pepper_27
 export NACL_TOOLS=${NACL_SDK_ROOT}/tools
-export DART=/home/jeffbailey/Programming/gclienttree/dart/dart/out/DebugIA32/dart-sdk/bin
-export PATH=${DART}:${NACL_SDK_ROOT}/toolchain/linux_x86_glibc/bin:${NACL_TOOLS}:${PATH}
+export PATH=${NACL_SDK_ROOT}/toolchain/linux_x86_glibc/bin:${NACL_TOOLS}:${PATH}
